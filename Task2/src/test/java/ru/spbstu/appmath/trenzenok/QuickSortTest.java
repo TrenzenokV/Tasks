0ca34b7aab4371904b6adf72c568439a275d8ef9
TrenzenokV <trenzenok-vova@mail.ru>
2015-11-09 17:17:54 +0200

    Commit

--- Task2/src/test/java/ru/spbstu/appmath/trenzenok/QuickSortTest.java
+++ Task2/src/test/java/ru/spbstu/appmath/trenzenok/QuickSortTest.java
@@ -26,38 +26,26 @@ public class QuickSortTest<T> {
     };
     private static final QuickSort QUICK_SORT = new QuickSort();
 
-    private static Object[][] TEST_DATA_DOUBLE = doubleDataInit();
-    private static Object[][] TEST_DATA_CARS = {
+    private static Object[][] TEST_DATA = {
             {QUICK_SORT, CAR_ENGINEPOWER_COMPARATOR, new Car[]{}},
             {QUICK_SORT, CAR_MODEL_COMPARATOR, new Car[]{new Car("mercedes", 2010, 220.8), new Car("audi", 2009, 450.5), new Car("bmw", 2011, 245)}},
             {QUICK_SORT, CAR_BIRTHYEAR_COMPARATOR, new Car[]{new Car("toyota", 2015, 150.7), new Car("nissan", 2014, 300)}},
-            {QUICK_SORT, CAR_ENGINEPOWER_COMPARATOR, new Car[]{new Car("honda", 2003, 100), new Car("mazda", 2007, 98.7), new Car("ferrari", 2003, 480)}}
+            {QUICK_SORT, CAR_ENGINEPOWER_COMPARATOR, new Car[]{new Car("honda", 2003, 100), new Car("mazda", 2007, 98.7), new Car("ferrari", 2003, 480)}},
+            {QUICK_SORT, DOUBLE_COMPARATOR,doubleDataInit()}
     };
 
-    private static Object[][] doubleDataInit() {
-        int size = 4;
-        Object[][] data = new Object[size][3];
+    private static Double[] doubleDataInit() {
+        Double[] array = new Double[4];
         Random random = new Random();
-        int numOfElem;
-        Double[] array;
-        for (int i = 0; i < size; i++) {
-            numOfElem = random.nextInt(50);
-            array = new Double[numOfElem];
-            for (int j = 0; j < array.length; j++) {
-                array[j] = Math.floor(random.nextDouble() * 200.0) / 30.0 - 5.0;
-            }
-            data[i][0] = QUICK_SORT;
-            data[i][1] = DOUBLE_COMPARATOR;
-            data[i][2] = array;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.floor(random.nextDouble() * 200.0) / 30.0 - 5.0;
         }
-
-        return data;
+        return array;
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> testData() {
-          return Arrays.asList(TEST_DATA_CARS);
-        //return  Arrays.asList(TEST_DATA_DOUBLE);
+          return Arrays.asList(TEST_DATA);
     }
 
     private Sort<T> sort;
@@ -78,6 +66,7 @@ public class QuickSortTest<T> {
         Assert.assertTrue("The array isn't sorted", testAscendingOrder(result, comparator));
         Assert.assertEquals("Result array length should be equal to original", input.length, result.length);
         Assert.assertTrue(hasEachElementOf(input, result, comparator));
+        Assert.assertTrue("Input array differs from Result array", hasSameNumberOfEachElement(input, result, comparator));
     }
 
     private boolean testAscendingOrder(T[] array, Comparator<T> comparator) {
@@ -100,4 +89,21 @@ public class QuickSortTest<T> {
         return true;
     }
 
+    private boolean hasSameNumberOfEachElement(T[] input, T[] result, Comparator<T> comparator) {
+        for(T element : input){
+            int numOfElementInInput = 0;
+            int numOfElementInResult = 0;
+            for(int j = 0; j < input.length; ++j){
+                if(comparator.compare(element,input[j]) == 0)
+                    numOfElementInInput++;
+            }
+            for(int i = 0; i < result.length; ++i){
+                if(comparator.compare(element,result[i]) == 0)
+                    numOfElementInResult++;
+            }
+            if(numOfElementInInput != numOfElementInResult)
+                return false;
+        }
+        return true;
+    }
 }
